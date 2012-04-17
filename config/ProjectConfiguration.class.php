876b27c72057cc3e6391885bc487ad9675972e6e
Soulou <leo.unbek@gmail.com>
2012-04-17 14:41:36 +0200

    Cration du projet symfony

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/vendor/symfony-1.4.17/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
