b056c33c9de3ebbe7b027c0d95cc57bfcddbe05c
Soulou <leo.unbek@gmail.com>
2012-04-24 23:23:27 +0200

    From Student table to User AND Role tables

---
+++ src/Unsapa/IPWBundle/Entity/Role.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Unsapa\IPWBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Unsapa\IPWBundle\Entity\Role
+ */
+class Role
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
