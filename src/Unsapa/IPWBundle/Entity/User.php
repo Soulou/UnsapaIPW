f703166d98e6989cf8bd441bbc8fa15a21f366d2
Soulou <leo.unbek@gmail.com>
2012-04-24 23:23:27 +0200

    From Student table to User AND Role tables

---
+++ src/Unsapa/IPWBundle/Entity/User.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace Unsapa\IPWBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Unsapa\IPWBundle\Entity\User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer $id
+     */
+    protected $id;
+
+    /**
+     * @var string $lastname
+     */
+    private $lastname;
+
+    /**
+     * @var string $firstname
+     */
+    private $firstname;
+
+    /**
+     * @var string $address
+     */
+    private $address;
+
+    /**
+     * @var string $zipcode
+     */
+    private $zipcode;
+
+    /**
+     * @var string $city
+     */
+    private $city;
+
+    /**
+     * @var string $phone
+     */
+    private $phone;
+
+    /**
+     * @var Unsapa\IPWBundle\Entity\Role
+     */
+    private $role;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return User
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set zipcode
+     *
+     * @param string $zipcode
+     * @return User
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+        return $this;
+    }
+
+    /**
+     * Get zipcode
+     *
+     * @return string 
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set role
+     *
+     * @param Unsapa\IPWBundle\Entity\Role $role
+     * @return User
+     */
+    public function setRole(\Unsapa\IPWBundle\Entity\Role $role = null)
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return Unsapa\IPWBundle\Entity\Role 
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
