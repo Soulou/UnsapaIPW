749363f13dab0a8f1824d9f8d7e66d36fb1be5e3
Soulou <leo.unbek@gmail.com>
2012-05-01 16:56:08 +0200

    Change variable name /exams

--- src/Unsapa/IPWBundle/Controller/ExamsController.php
+++ src/Unsapa/IPWBundle/Controller/ExamsController.php
@@ -3,24 +3,56 @@
 namespace Unsapa\IPWBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Unsapa\IPWBundle\Entity\Exam;
 
 class ExamsController extends Controller
 {
     public function addAction()
     {
-      $promos = $this->getDoctrine()
-        ->getRepository('UnsapaIPWBundle:Promo')
-        ->findAll();
+      $exam = new Exam();
+      $request = $this->getRequest();
+      if(count($request->request) != 0)
+      {
+        $validator = $this->get('validator');
 
-      return $this->render('UnsapaIPWBundle:Exams:add.html.twig', array('promos' => $promos));
+        $promo = $this->getDoctrine()
+          ->getRepository('UnsapaIPWBundle:Promo')
+          ->findOneByName($request->get('name'));
+
+        $exam->setTitle($request->get('title'));
+        $exam->setPromo($promo);
+        $exam->setExamDesc($request->get('desc'));
+        $exam->setExamDate(new \DateTime($request->get('date')));
+        $exam->setCoef($request->get('coef'));
+        $exam->setResp($this->get('security.context')->getToken()->getUser());
+
+        $errors = $validator->validate($exam);
+
+        if (count($errors) > 0)
+          return new Response(print_r($errors, true));
+        else
+        {
+          $manager = $this->get('doctrine')->getEntityManager();
+          $manager->persist($exam);
+          $manager->flush();
+          return $this->redirect($this->generateUrl('exams'), 201);
+        }
+      }
+      else
+      {
+        $promos = $this->getDoctrine()
+          ->getRepository('UnsapaIPWBundle:Promo')
+          ->findAll();
+        return $this->render('UnsapaIPWBundle:Exams:add.html.twig', array('promos' => $promos,'exam' => $exam));
+      }
     }
     public function indexAction()
     {
       $user = $this->get('security.context')->getToken()->getUser();
-      $exams = $this->getDoctrine()
+      $records = $this->getDoctrine()
         ->getRepository('UnsapaIPWBundle:Record')
-        ->findByUser($user);
+        ->findByStudent($user);
 
-      return $this->render('UnsapaIPWBundle:Exams:index.html.twig', array('exams' => $exams));
+      return $this->render('UnsapaIPWBundle:Exams:index.html.twig', array('exam' => $records));
     }
 }
