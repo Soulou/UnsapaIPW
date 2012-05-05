f11fcbcb5b4ac6caee328672a1ef765e414d6d38
guparan <guparan@latitude.localdomain>
2012-05-05 19:59:58 +0200

    Modification in file structure : PromosController creation

--- src/Unsapa/IPWBundle/Controller/AdminController.php
+++ src/Unsapa/IPWBundle/Controller/AdminController.php
@@ -13,31 +13,6 @@ class AdminController extends Controller
 {
     public function indexAction()
     {
-    	$promos = $this->getDoctrine()->getRepository("UnsapaIPWBundle:Promo")->findAll();
-    	return $this->render('UnsapaIPWBundle:Admin:index.html.twig', array("promos"=>$promos));
-    }
-    
-    public function addAction(Request $request)
-    {
-    	$promo = new Promo();
-    	$form = $this->createFormBuilder($promo)
-    	->add('name', 'text', array('label' => "Promotion : "))
-    	->getForm();
-    	
-    	if($request->getMethod() == "POST")
-    	{
-    		$form->bindRequest($request);
-    	
-    		if($form->isValid())
-    		{
-    			$manager = $this->get('doctrine')->getEntityManager();
-    			$manager->persist($promo);
-    			$manager->flush();
-    			
-    			return $this->redirect($this->generateUrl('admin'), 201);
-    		}
-    	}
-    	return $this->render('UnsapaIPWBundle:Admin:add.html.twig', array('promo' => $promo, 'form' => $form->createView()));
-    	
+    	return $this->render('UnsapaIPWBundle:Admin:index.html.twig');
     }
 }
---
+++ src/Unsapa/IPWBundle/Controller/PromosController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Unsapa\IPWBundle\Controller;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Unsapa\IPWBundle\Entity\Record;
+use Unsapa\IPWBundle\Entity\Promo;
+
+class PromosController extends Controller
+{
+    public function indexAction()
+    {
+    	$promos = $this->getDoctrine()->getRepository("UnsapaIPWBundle:Promo")->findAll();
+    	return $this->render('UnsapaIPWBundle:Promos:index.html.twig', array("promos"=>$promos));
+    }
+    
+    public function addAction(Request $request)
+    {
+    	$promo = new Promo();
+    	$form = $this->createFormBuilder($promo)
+    	->add('name', 'text', array('label' => "Promotion : "))
+    	->getForm();
+    	
+    	if($request->getMethod() == "POST")
+    	{
+    		$form->bindRequest($request);
+    	
+    		if($form->isValid())
+    		{
+    			$manager = $this->get('doctrine')->getEntityManager();
+    			$manager->persist($promo);
+    			$manager->flush();
+    			
+    			return $this->redirect($this->generateUrl('promos'), 201);
+    		}
+    	}
+    	return $this->render('UnsapaIPWBundle:Promos:add.html.twig', array('promo' => $promo, 'form' => $form->createView()));
+    	
+    }
+}
