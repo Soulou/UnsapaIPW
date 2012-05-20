de2928fae73913fc08da38fb1922b2192b4dcd22
Soulou <leo.unbek@gmail.com>
2012-05-21 01:12:31 +0200

    Test the Exam and the Promo Entities

---
+++ src/Unsapa/IPWBundle/Tests/Helper/DoctrineTestHelper.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * DoctrineTestHelper.php
+ *
+ * @author leo@soulou.fr
+ * @package Unsapa\IPWBundle\Tests\Helper
+ */
+
+namespace Unsapa\IPWBundle\Tests\Helper;
+
+use Doctrine\ORM\Tools\SchemaTool;
+
+/**
+ * Some functions to use when testing persistency
+ */
+class DoctrineTestHelper
+{
+  /*
+   * Kernel of the application
+   * @var HttpKernelInterface $kernel
+   */
+  private $kernel;
+
+  /*
+   * EntityManager of Doctrine
+   * @var EntityManager $em
+   */
+  private $em;
+
+  /*
+   * Validator of Symfony
+   * @var Validator $validator
+   */
+  private $validator;
+
+  /**
+   * Main constructor
+   * @param HttpKernelInterface $kernel Kernel of the application
+   */
+  public function __construct($kernel)
+  {
+    if($kernel == NULL)
+      throw new \ErrorException("Kernel is null");
+
+    $this->kernel = $kernel;
+    $this->em = DoctrineTestHelper::getEntityManager();
+    $this->em = $this->kernel->getContainer()->get('doctrine.orm.entity_manager');
+    $this->validator = $this->kernel->getContainer()->get('validator');
+  }
+
+  /**
+   * Drop all the content, and reset the schema of the test database
+   */
+  public function resetDatabase()
+  {
+    $schema_manager = $this->em->getConnection()->getDriver()->getSchemaManager($this->em->getConnection());
+    $schema_tool = new SchemaTool($this->em);
+    $metadatas = $this->em->getMetadataFactory()->getAllMetadata();
+    $schema_tool->dropSchema($metadatas);
+    $schema_tool->createSchema($metadatas);
+  }
+
+  /**
+   * Get the entity manager of doctrine
+   * @return EntityManager Doctrine's entity manager
+   */
+  public function getEntityManager()
+  {
+    return $this->em;
+  }
+
+  /**
+   * Get the current validator
+   * @return Validator
+   */
+  public function getValidator()
+  {
+    return $this->validator;
+  }
+}
