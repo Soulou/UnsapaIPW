981432941cb1a02ce38045057e8ddaa8f55a3ee4
Soulou <leo.unbek@gmail.com>
2012-05-24 00:22:58 +0200

    Remove fos overriding and use our own form to modify user profile
    Fixed /users twig
    
    "Fixed /users twig [bis]"
    
    Filter set

--- src/Unsapa/IPWBundle/Form/Type/ProfileFormType.php
+++ src/Unsapa/IPWBundle/Form/Type/ProfileFormType.php
@@ -13,31 +13,32 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\FormBuilder;
-use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+use Symfony\Component\Form\AbstractType;
 
 /**
  * Override the ProfileFormType of the FOSUserBundle to add fields to the form
  */
-class ProfileFormType extends BaseType
+class ProfileFormType extends AbstractType
 {
     /**
      * Build user profile form
      * @param FormBuilder $builder
      * @param array $options
      */
-    public function buildUserForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilder $builder, array $options)
     {
         $builder
             ->add('email')
             ->add('lastname', 'text', array('required'=>true))
             ->add('firstname', 'text', array('required'=>true))
-        	->add('promo')
+        	  ->add('promo')
             ->add('address')
             ->add('zipcode')
             ->add('city')
             ->add('phone')
         ;
     }
+
     /**
      * Get form name
      */
