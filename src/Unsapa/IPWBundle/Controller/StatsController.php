70eb43f235ac3ecacd362d6f0c70497661160dd9
Soulou <leo.unbek@gmail.com>
2012-05-23 16:41:40 +0200

    Sort by date the exams of the stats page

--- src/Unsapa/IPWBundle/Controller/StatsController.php
+++ src/Unsapa/IPWBundle/Controller/StatsController.php
@@ -30,7 +30,7 @@ class StatsController extends Controller
     {
         $promos = $this->getDoctrine()->getRepository("UnsapaIPWBundle:Promo")->findAll();
         $exams_ended = $this->getDoctrine()->getEntityManager()->createQuery(
-            "SELECT e FROM UnsapaIPWBundle:Exam e WHERE e.exam_date < :date")
+            "SELECT e FROM UnsapaIPWBundle:Exam e WHERE e.exam_date < :date ORDER BY e.exam_date DESC")
             ->setParameter("date", (new \Datetime('now')))
             ->getResult();
 
